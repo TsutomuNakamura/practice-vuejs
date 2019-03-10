f92e7b5f172fb29be0bc85c6c28f40a3690ab64c
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-03-10 22:36:39 +0900

    Added new resource

--- 00_IntroToVueJs/08_Components/main.js
+++ 00_IntroToVueJs/08_Components/main.js
@@ -1,4 +1,10 @@
 Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
   template: `
     <div class="product">
 
@@ -11,6 +17,7 @@ Vue.component('product', {
 
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
 
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
@@ -75,10 +82,19 @@ Vue.component('product', {
     },
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      if(this.premium) {
+        return "Free";
+      }
+      return 2.99;
     }
   }
 });
 
 var app = new Vue({
   el: '#app',
+  data: {
+    premium: true
+  }
 });
